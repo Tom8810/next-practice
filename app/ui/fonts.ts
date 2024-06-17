aa822085817a3e2299dfaf88414d96c18f6d706f
Tomsoya8810 <soyan8810@gmail.com>
2024-06-17 18:56:59 +0900

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Alata } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
+export const alata = Alata({ subsets: ['latin'], weight: ['400'] });
